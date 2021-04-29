631a4b68079fd659fb6f50c99ad57dd2f9a7053e
Alicia A Taylor <aliciataylorguitar@gmail.com>
2021-04-29 13:48:14 -0500

    Class 73.5

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,6 +14,13 @@
 //console.log(document.querySelector('.guess').value);
 
 document.querySelector('.check').addEventListener('click', function() {
-    console.log(document.querySelector('.guess').value);
+    const guess = Number (document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = '⛔ No number!';
+        
+    }
 });
+
 
