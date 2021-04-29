f751c4006b6a5e9eae91b8c034802d7e5f6341c3
Alicia A Taylor <aliciataylorguitar@gmail.com>
2021-04-29 10:35:47 -0500

    Class 72

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,4 +4,12 @@
 // .message is the CLASS just like in CSS... textContent displays the properties content only instead of the tags with it
 //console.log(document.querySelector('.message').textContent);
 
+//change in the dom not just console
+//document.querySelector('.message').textContent = 'Correct Number!';
+
+//document.querySelector('.number').textContent = 13;
+//document.querySelector('.score').textContent = 10;
+
+//document.querySelector('.guess').value = 23;
+//console.log(document.querySelector('.guess').value);
 
