f7b21b7498a8d5458f7c3a5f8e30216b6c49fa02
Alicia A Taylor <aliciataylorguitar@gmail.com>
2021-04-29 10:25:26 -0500

    Class 71

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,7 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+//class 70 manipulate the dom intro
+// .message is the CLASS just like in CSS... textContent displays the properties content only instead of the tags with it
+//console.log(document.querySelector('.message').textContent);
+
 
