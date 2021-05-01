bf7f4a490addbd6e351afb403c51089ed7211ad5
Alicia A Taylor <aliciataylorguitar@gmail.com>
2021-04-30 19:42:21 -0500

    Class 80 - Classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,10 +1,29 @@
 'use strict';
 
-const moodal = document.querySelector('.modal');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
+
+// console.log(btnOpenModal);
 
 for (let i = 0; i < btnOpenModal.length; i++)
-console.log(btnOpenModal[i].textContent);
+btnOpenModal[i].addEventListener('click', function() {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+});
+
+const closeModal = function() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e) {
+    console.log(e.key);
+    if(e.key === 's' && !modal.classList.contains('hidden')) {       
+        closeModal();
+    }
+});
